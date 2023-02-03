fb64e96f478aea60ef6ddef12e635197209d0e5f
will-ks <will@willks.com>
2023-02-03 22:05:08 +0100

    Add convertToString

--- src/json-helpers.ts
+++ src/json-helpers.ts
@@ -7,20 +7,32 @@ export const isValidJSONString = (str: string) => {
   return true;
 };
 
-export const circularReferenceSafeJSONStringify = (toStringify: any) => {
-  return JSON.stringify(
-    toStringify,
-    (() => {
-      const visited = new WeakSet();
-      return (_key: any, value: any) => {
-        if (visited.has(value)) {
-          return '[Circular reference]';
-        }
-        if (typeof value === 'object' && value !== null) {
-          visited.add(value);
-        }
-        return value;
-      };
-    })()
+// A better JSON.stringify. Safely stringify circular references, Errors and undefined.
+export const convertToString = (toStringify: unknown) => {
+  return (
+    JSON.stringify(
+      toStringify,
+      (() => {
+        const visited = new WeakSet();
+        return (_key: any, value: any) => {
+          if (value instanceof Error) {
+            value = Object.getOwnPropertyNames(value).reduce(
+              (acc, propertyName) => ({
+                ...acc,
+                [propertyName]: value[propertyName],
+              }),
+              {}
+            );
+          }
+          if (visited.has(value)) {
+            return '[Circular reference]';
+          }
+          if (typeof value === 'object' && value !== null) {
+            visited.add(value);
+          }
+          return value;
+        };
+      })()
+    ) ?? 'undefined'
   );
 };
