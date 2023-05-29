481e817b111d624aaf15ea46aaeb9b2f02983197
will-ks <will@willks.com>
2023-05-29 19:00:53 +0200

    Refactor assertion helper

--- src/type-helpers.ts
+++ src/type-helpers.ts
@@ -22,9 +22,30 @@ export const isUnknownObject = (toCheck: unknown): toCheck is UnknownObject => {
   return typeof toCheck === 'object';
 };
 
-export const assertAndReturn = <T>(itemToAssert?: T, itemName = 'Object') => {
-  if (!itemToAssert) {
-    throw new Error(`Expected ${itemName}`);
-  }
-  return itemToAssert as NonNullable<T>;
+/**
+ * Takes one or more nullable variables as arguments.
+ * If any of the arguments are null or undefined, throws a runtime error.
+ * Otherwise, returns the arguments with their type asserted as NonNullable.
+ * @throws {Error} If any argument is null or undefined.
+ */
+export function getAsserted<T>(arg: T): NonNullable<T>;
+export function getAsserted<T extends unknown[]>(
+  ...args: T[]
+): {
+  [K in keyof T]: T[K] extends null | undefined ? never : NonNullable<T[K]>;
 };
+export function getAsserted<T extends unknown[]>(...args: unknown[]) {
+  args.forEach((arg, ix) => {
+    if (arg === null || arg === undefined) {
+      throw new Error(`Argument at index ${ix} is null or undefined`);
+    }
+  });
+
+  return args.length === 1
+    ? (args[0] as NonNullable<T[0]>)
+    : (args as {
+        [K in keyof T]: T[K] extends null | undefined
+          ? never
+          : NonNullable<T[K]>;
+      });
+}
